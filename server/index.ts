daa8fd884fe30fe786217190535b811e7a3b8053
IT-imataka <takashi6128_gakky@outlook.com>
2025-12-23 02:08:05 +0900

    fix cors index.ts slash delete

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://tic-tac-toe-client-brp5.onrender.com/",
+      "https://tic-tac-toe-client-brp5.onrender.com",
     ],
     methods: ["GET", "POST"],
   },
