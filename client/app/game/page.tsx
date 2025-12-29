9a4cba82c6dd67e172c9b256aa53e01b7a738ecf
IT-imataka <takashi6128_gakky@outlook.com>
2025-12-29 14:45:22 +0900

    redering compornent wrapped Suspense

--- client/app/game/page.tsx
+++ client/app/game/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect,useState } from 'react';
+import { Suspense,useEffect,useState } from 'react';
 import { useSearchParams,useRouter } from 'next/navigation';
 import { io,Socket } from 'socket.io-client';
 import { GlowingCard } from '../page';
@@ -78,63 +78,66 @@ export default function Game() {
     
     return (
       // 以下はページ全てを返すjsx
-      <div className="flex min-h-screen flex-col items-center justify-center bg-slate-950 p-4">
-        <h1 className="text-4xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-400">消える〇✖ゲーム</h1>
-        <p className="pt text-white-600 pb-4">交互に配置されます</p>
-        {/* グリッドの生成 */}
-        <div className="grid grid-cols-3 gap-2 bg-white-500 p-2 rounded-l ">
-          {board.map((cel,index) => {
-            // 消えるギミック判定のflagを立てる
-            let invisibleflag = false;
-            if(isNext){
-              // 各手番、最初に置かれた駒を判定
-              if(oMoves.length >= 3 && oMoves[0] === index){
-                invisibleflag = true;
+      <Suspense fallback={
+        <div className="flex min-h-screen flex-col items-center justify-center bg-slate-950 p-4">
+          <h1 className="text-4xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-400">消える〇✖ゲーム</h1>
+          <p className="pt text-white-600 pb-4">交互に配置されます</p>
+          {/* グリッドの生成 */}
+          <div className="grid grid-cols-3 gap-2 bg-white-500 p-2 rounded-l ">
+            {board.map((cel,index) => {
+              // 消えるギミック判定のflagを立てる
+              let invisibleflag = false;
+              if(isNext){
+                // 各手番、最初に置かれた駒を判定
+                if(oMoves.length >= 3 && oMoves[0] === index){
+                  invisibleflag = true;
+                }
+                if(xMoves.length >= 3 && xMoves[0] === index){
+                  invisibleflag = true;
+                }
               }
-              if(xMoves.length >= 3 && xMoves[0] === index){
-                invisibleflag = true;
-              }
-            }
-            // console.log("生のnextInv:", nextInv)
-            // 以下はボタンを返すためのjsx
-            return (  
-              <div key={index} 
-              className={`relative p-[1px] rounded-xl overflow-hidden transition-all duration-500 ${invisibleflag ? 'opacity-40 scale-95' : 'opacity-100'}`}>
-                <div className={`inset-0 bg-gradient-to-br from-cyan-500 via-purple-500 to-pink-500
-                  ${invisibleflag ? 'blur-[1]' : 'blur-[5] opacity-50'}`}>
-                  <button
-                  className={`
-                     relative w-24 h-24 bg-slate-900 rounded-xl text-5xl font-bold flex items-center justify-center rounded-lg  transition-all duration-300 transform active:scale-95 transition-colors
-                  
-                     ${!cel && !winner ? 'hover:bg-slate-800' : ''}
-                     ${!cel ? 'bg-black-700 hover:bg-black-600' : 'bg-white-600'}
-                     ${invisibleflag ? 'opacity-30 border-2 border-dashed border-red-400' : 'opacity-100'}
-                    `}
-                  key={index}
-                  onClick={() => handlcelClick(index)}>
-                  {/* nullなら空文字　‘〇'なら〇 */}
-                  {cel === '○' && <span className='text-cyan-300 drop-shadow-[0_0_10px_rgba(96,165,250,0.8)]'>○</span>}
-                  {cel === '×' && <span className='text-pink-500 drop-shadow-[0_0_10px_rgba(244,114,182,0.8)]'>×</span>}
-                  </button>
+              // console.log("生のnextInv:", nextInv)
+              // 以下はボタンを返すためのjsx
+              return (
+                <div key={index}
+                className={`relative p-[1px] rounded-xl overflow-hidden transition-all duration-500 ${invisibleflag ? 'opacity-40 scale-95' : 'opacity-100'}`}>
+                  <div className={`inset-0 bg-gradient-to-br from-cyan-500 via-purple-500 to-pink-500
+                    ${invisibleflag ? 'blur-[1]' : 'blur-[5] opacity-50'}`}>
+                    <button
+                    className={`
+                       relative w-24 h-24 bg-slate-900 rounded-xl text-5xl font-bold flex items-center justify-center rounded-lg  transition-all duration-300 transform active:scale-95 transition-colors
+        
+                       ${!cel && !winner ? 'hover:bg-slate-800' : ''}
+                       ${!cel ? 'bg-black-700 hover:bg-black-600' : 'bg-white-600'}
+                       ${invisibleflag ? 'opacity-30 border-2 border-dashed border-red-400' : 'opacity-100'}
+                      `}
+                    key={index}
+                    onClick={() => handlcelClick(index)}>
+                    {/* nullなら空文字　‘〇'なら〇 */}
+                    {cel === '○' && <span className='text-cyan-300 drop-shadow-[0_0_10px_rgba(96,165,250,0.8)]'>○</span>}
+                    {cel === '×' && <span className='text-pink-500 drop-shadow-[0_0_10px_rgba(244,114,182,0.8)]'>×</span>}
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
+          {winner && (
+            <h2 className='mt-4 mb-6 text-2xl font-bold text-white animate-bounce'>
+              <span className={winner === '○' ? 'text-cyan-400' : 'text-pink-500'}>
+                {winner === "○" ? "○" : "×"}の方が勝利です！おめでとうございます！
+              </span>
+            </h2>
+            )}
+            {
+              inviteURL && (
+                <h2><a className='hover:underline hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-400' target='_blank' href={`${inviteURL}`}>{inviteURL}</a>&nbsp;を送ってね！</h2>
+              )
+            }
+          <button onClick={handleReset} className="mt-10 text-slate-300 transition">
+            <GlowingCard className='animate-pulse hover:animate-none'>リセットボタン</GlowingCard></button>
         </div>
-        {winner && (
-          <h2 className='mt-4 mb-6 text-2xl font-bold text-white animate-bounce'>
-            <span className={winner === '○' ? 'text-cyan-400' : 'text-pink-500'}>
-              {winner === "○" ? "○" : "×"}の方が勝利です！おめでとうございます！
-            </span>
-          </h2>
-          )}
-          {
-            inviteURL && (
-              <h2><a className='hover:underline hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-400' target='_blank' href={`${inviteURL}`}>{inviteURL}</a>&nbsp;を送ってね！</h2>
-            )
-          }
-        <button onClick={handleReset} className="mt-10 text-slate-300 transition">
-          <GlowingCard className='animate-pulse hover:animate-none'>リセットボタン</GlowingCard></button>
-      </div>
+      }>
+      </Suspense>
     );
 }
