8b83152fed8f4b43720f0a82b2b47dd49aa24a21
IT-imataka <takashi6128_gakky@outlook.com>
2025-12-30 22:24:53 +0900

    fix style

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -32,7 +32,7 @@ export function GlowingCard({ children, className = "", }: { children: React.Rea
 // ボタンのコンポーネント
 export function RoomBtn({ children, className = "", onClick = () => { }, }: { children: React.ReactNode; className?: string; onClick?: () => void; }) {
   return (
-    <button className="text-white transition-transform active:scale-95" onClick={onClick}> {children}</button >
+    <button className="text-white transition-transform active:scale-85" onClick={onClick}> {children}</button >
   )
 }
 
