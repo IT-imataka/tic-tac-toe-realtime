445a02c423a1ceafb528f653845684f66a5b0c85
IT-imataka <takashi6128_gakky@outlook.com>
2026-01-19 23:20:07 +0900

    fix port number

--- server/index.ts
+++ server/index.ts
@@ -215,7 +215,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 || 8000;
 server.listen(PORT, () => {
   console.log(`🚀 サーバー起動中: http://localhost:${PORT}`);
 });
